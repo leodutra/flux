d5323dd512a207faabe7143a5fa64a7c710df268
Leonardo Dutra <leodutra.br@gmail.com>
2012-05-21 22:02:16 +0000

    Update

---
+++ display/Displayable.js
@@ -0,0 +1,23 @@
+/*
+ * Flux JavaScript Library
+ * Copyright (c) 2010 Leonardo Dutra Constancio
+ * MIT License (http://jsflux.googlecode.com/svn/trunk/mit-license.txt)
+ */
+
+flux.Displayable = function ()
+{
+    if (this instanceof flux.Displayable)
+    {
+        flux.Object2D.call(this);
+        this.transform = null;
+        // TODO adjust transform for HTML5 Canvas
+        this.composite = flux.CompositeMode.SOURCE_OVER;
+        // ~= AS3 blendMode
+        // this.graphics = new flux.Graphics();
+        // TODO cacheAsBitmap (AS3)?
+        // TODO filters?
+        // TODO mask?
+    }
+};
+
+flux.Displayable.prototype = new flux.Object2D;
---
+++ display/Shape2D.js
@@ -0,0 +1,16 @@
+/*
+ * Flux JavaScript Library
+ * Copyright (c) 2010 Leonardo Dutra Constancio
+ * MIT License (http://jsflux.googlecode.com/svn/trunk/mit-license.txt)
+ */
+
+flux.Shape2D = function ()
+{
+    if (this instanceof flux.Shape2D)
+    {
+        this.geometry = new flux.Geometry();
+    }
+};
+flux.Shape2D.prototype = {
+
+};
