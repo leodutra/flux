108c530865e8b988283406592935cf99d6c764cc
Leonardo Dutra <leodutra.br@gmail.com>
2012-05-22 00:13:13 +0000

    Fix licence uri

--- core/RGBA.js
+++ core/RGBA.js
@@ -1,7 +1,7 @@
 /*
  * Flux JavaScript Library
  * Copyright (c) 2010 Leonardo Dutra Constancio
- * MIT License (http://jsflux.googlecode.com/svn/trunk/MIT-license.txt)
+ * MIT License (http://jsflux.googlecode.com/svn/trunk/mit-license.txt)
  */
 
 
